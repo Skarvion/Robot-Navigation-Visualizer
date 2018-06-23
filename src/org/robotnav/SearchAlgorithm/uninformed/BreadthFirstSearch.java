75ee146b147a55a7be1d6896f9f97b8cee3b0fee
Skarvion <williams.saputra@gmail.com>
2018-06-23 14:00:24 +1000

    commented

--- src/org/robotnav/SearchAlgorithm/uninformed/BreadthFirstSearch.java
+++ src/org/robotnav/SearchAlgorithm/uninformed/BreadthFirstSearch.java
@@ -2,35 +2,49 @@ package org.robotnav.SearchAlgorithm.uninformed;
 
 import org.robotnav.Robot.Direction;
 import org.robotnav.Robot.Robot;
-import org.robotnav.SearchAlgorithm.BranchedTree.BranchedNode;
 import org.robotnav.SearchAlgorithm.Data.ActionNode;
 import org.robotnav.SearchAlgorithm.Data.Coordinate;
 import org.robotnav.SearchAlgorithm.SearchAlgorithm;
 import org.robotnav.SearchAlgorithm.Tree.Node;
 import org.robotnav.SearchAlgorithm.Tree.Tree;
-import org.robotnav.map.Cell;
 import org.robotnav.map.CellType;
 import org.robotnav.map.Grid;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Breadth first search traverse all of the frontier equally and thus the search tree has a much wider shape
+ */
 public class BreadthFirstSearch extends SearchAlgorithm {
 
+    /**
+     * Constructor set the ID of this algorithm
+     */
     public BreadthFirstSearch() {
         setSearchAlgorithmName(new String[]{"Breadth First Search", "BFS", "BreadthFirstSearch"});
     }
 
+    /**
+     * Override execute search method for BFS
+     * @param gridMap map to be traversed
+     * @param robot robot in map
+     * @return path direction, null if no solution found
+     */
     @Override
     public ArrayList<Direction> executeSearch(Grid gridMap, Robot robot) {
+
+        // Create search tree, frontier list and root node
         Tree<ActionNode> searchTree = new Tree<>(new ActionNode(Direction.None, new Coordinate(robot.getxCoordinate(), robot.getyCoordinate())));
         LinkedList<Node<ActionNode>> frontiers = new LinkedList<>();
         frontiers.add(searchTree.getRootNode());
         setNodeCount(1);
 
+        // Loop while frontier is not empty, poll the first entry
         Node<ActionNode> selectedNode;
         while ((selectedNode = frontiers.pollFirst()) != null) {
+
+            // If solution is found, return result path
             if (gridMap.getCell(selectedNode.getData().getCoordinate().getX(), selectedNode.getData().getCoordinate().getY()).getType() == CellType.Goal) {
                 return deriveSolutionFromNode(selectedNode);
             }
@@ -43,6 +57,7 @@ public class BreadthFirstSearch extends SearchAlgorithm {
             updateCellState(xCoordinate, yCoordinate, CellType.Visited);
             ArrayList<Direction> possibleDirections = visitableDirection(gridMap, xCoordinate, yCoordinate, sourceDirection);
 
+            // For each possible direction, add to frontier
             for (Direction d : possibleDirections) {
                 int newXPos = xCoordinate;
                 int newYPos = yCoordinate;
